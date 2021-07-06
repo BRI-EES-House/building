8023d009782e353a2417fb336bf4d69722ef4585
Hisashi Miura <hisashi.office@gmail.com>
2021-07-07 00:10:25 +0900

    バージョンをインクリメント

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Building Research Institute'
 author = 'BRI'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
