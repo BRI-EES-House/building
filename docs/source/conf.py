07eb009833387a1b77ddefb67336e7dd77d9c312
Hisashi Miura <hisashi.office@gmail.com>
2021-07-20 18:31:30 +0900

    paper size を変更する

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,6 +59,8 @@ latex_elements = {
 '''
 }
 
+latex_paper_size = 'a4'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
